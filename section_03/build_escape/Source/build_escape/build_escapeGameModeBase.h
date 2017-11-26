0cc6f0e5339c5df8ee3e19164be2d97810feaa47
shuyangsun_desktop <sunbuffett_gmail_com>
2017-11-26 14:30:23 -0500

    Add initialized UE4 building escape project.

---
+++ section_03/build_escape/Source/build_escape/build_escapeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "build_escapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILD_ESCAPE_API Abuild_escapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
