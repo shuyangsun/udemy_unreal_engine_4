e2c47709d37159a7d69b0ea38cada91f7d44bad4
shuyangsun_desktop <sunbuffett_gmail_com>
2017-11-27 22:47:24 -0500

    Add PositionReport class.

--- section_03/build_escape/Source/build_escape/PositionReport.h
+++ section_03/build_escape/Source/build_escape/PositionReport.h
@@ -24,6 +24,7 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+  float _delta_time{};
 	
 };
